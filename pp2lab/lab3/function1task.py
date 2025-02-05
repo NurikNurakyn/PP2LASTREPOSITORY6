15321846745414ba72f419ccd486e79ed23bc436
NurikNurakyn <nurakynn35@gmail.com>
2025-02-05 23:11:41 +0500

    Обновление код

--- pp2lab/lab3/function1task.py
+++ pp2lab/lab3/function1task.py
@@ -1,18 +1,23 @@
-'''#1
+'''#1 A recipe you are reading states how many grams you need for the ingredient. Unfortunately, your store only sells items in ounces. Create a function to convert grams to ounces. ounces = 28.3495231 * grams
+
 def convert(grams):
     ounces = grams * 28.3495231
     return ounces
 n = float(input("Enter grams to convert: "))
 print(f"{n} grams =",round(convert(n),2),'ounces')
 
-#2
+
+#2 Read in a Fahrenheit temperature. Calculate and display the equivalent centigrade temperature. The following formula is used for the conversion: C = (5 / 9) * (F – 32)
+
 def f_to_c(F):
     C = (5 / 9) * (F - 32)
     print(F,"Fahrenheit =",round(C,2), 'Celcium')
 n1= float(input("Enter temperature in Fahrenheit: "))
 f_to_c(n1) 
 
-#3
+
+#3 Write a program to solve a classic puzzle: We count 35 heads and 94 legs among the chickens and rabbits in a farm. How many rabbits and how many chickens do we have? create function: solve(numheads, numlegs):
+
 def solve(numheads,numlegs):
     for i in range(numheads+1):
         j = numheads - i
@@ -24,7 +29,9 @@ def solve(numheads,numlegs):
 i1 = int(input('Heads count : '));j1 = int(input("Legs count : "))
 solve(i1,j1)
 
-#4
+
+#4 You are given list of numbers separated by spaces. Write a function filter_prime which will take list of numbers as an agrument and returns only prime numbers from the list.
+
 def prime(list):
     list1 = []
     for i in range(len(list)):
@@ -39,4 +46,27 @@ def prime(list):
             list1.append(list[i])
     return list1
 list2 = list(map(int, input().split()))
-print(prime(list2))'''
+print(prime(list2))
+
+
+#6 Write a function that accepts string from user, return a sentence with the words reversed. We are ready -> ready are We
+
+def reverse1(l):
+    l.reverse()
+    print(l)
+l1 = []
+for i in input().split():
+    l1.append(i)
+reverse1(l1)
+'''
+
+#7 Given a list of ints, return True if the array contains a 3 next to a 3 somewhere.
+def has_33(nums):
+    a = False
+    for i in range(len(nums)-1):
+        if (nums[i]==3 and (nums[i+1]==3)):
+            a = True
+    return a
+
+ns = list(map(int,input().split()))
+print(has_33(ns))
