cf9a9696355c1a783be02af1b7c320e7cd95caf7
NurikNurakyn <nurakynn35@gmail.com>
2025-02-06 00:27:32 +0500

    Обновление код

--- pp2lab/lab3/function1task.py
+++ pp2lab/lab3/function1task.py
@@ -58,7 +58,7 @@ l1 = []
 for i in input().split():
     l1.append(i)
 reverse1(l1)
-'''
+
 
 #7 Given a list of ints, return True if the array contains a 3 next to a 3 somewhere.
 def has_33(nums):
@@ -67,6 +67,77 @@ def has_33(nums):
         if (nums[i]==3 and (nums[i+1]==3)):
             a = True
     return a
-
+    
 ns = list(map(int,input().split()))
 print(has_33(ns))
+
+
+#8 Write a function that takes in a list of integers and returns True if it contains 007 in order
+def spy_game(nums):
+    a = False
+    for i in range(2,len(nums)):
+        if nums[i]==7 and nums[i-1]==0 and nums[i-2]==0:
+            a = True
+    return a
+ns1 = list(map(int,input().split()))
+print(spy_game(ns1))
+
+
+#9 Write a function that computes the volume of a sphere given its radius.
+def vol_of_sphere(r):
+    vol = (4/3)*(r**3)
+    print(round(vol,3))
+r = float(input())
+vol_of_sphere(r)
+
+
+#10 Write a Python function that takes a list and returns a new list with unique elements of the first list. Note: don't use collection set.
+def unique_list(list):
+    uniq = []
+    for i in list:
+        if i not in uniq:
+            uniq.append(i)
+    return uniq
+unique = list(map(int,input().split()))
+print(unique_list(unique))
+
+
+#11 Write a Python function that checks whether a word or phrase is palindrome or not. Note: A palindrome is word, phrase, or sequence that reads the same backward as forward, e.g., madam
+def is_palindrome(s):
+    if s == s[::-1]:
+        return "Palindrome"
+    else:
+        return "Not palindrome"
+
+is_pal = input()
+print(is_palindrome(is_pal))
+
+
+#12 Define a functino histogram() that takes a list of integers and prints a histogram to the screen. For example, histogram([4, 9, 7]) should print the following:
+def histogram(list):
+    for i in range(len(list)):
+        print(list[i]*'*')
+
+histog = list(map(int,input().split()))
+histogram(histog)
+
+#13 Write a program able to play the "Guess the number" - game, where the number to be guessed is randomly chosen between 1 and 20. This is how it should work when run in a terminal:
+from random import randint
+def guess_the_number():
+    x = -1
+    a = randint(1,20)
+    name = input("Hello! What is your name?\n")
+    print(f"Well, {name}, I am thinking of a number between 1 and 20.\nTake a guess.")
+    g = 0
+    while x != a:
+        g+=1
+        x = int(input())
+        if x>a:
+            print("Your guess is too big.\nTake a guess.")
+        elif x<a:
+            print("Your guess is too low.\nTake a guess.")
+        else:
+            print(f'Good job, KBTU! You guessed my number in {g} guesses!')
+
+guess_the_number()
+'''
